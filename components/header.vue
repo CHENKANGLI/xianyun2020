1154ee14d0f4e68966cb132d281abec6bfe59ced
陈康力 <1141132475@qq.com>
2020-02-07 23:58:01 +0800

    项目初始化完成

---
+++ components/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+      <el-row>
+          
+      </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Header />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+<script>
+   import Header from '@/components/header'
+   import Footer from '@/components/footer'
+
+   export default {
+     components:[
+       Header,Footer
+     ]
+   }
+</script>
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+a:hover {
+  color: inherit;
+}
+ul,li,ol {
+  list-style: none;
+}
+em,i {
+  font-style: normal;
+}
+
+</style>
