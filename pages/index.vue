76121bdbf27719188a8ebaf45545422ba9e779e1
陈康力 <1141132475@qq.com>
2020-02-08 23:29:17 +0800

    轮播图完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,53 @@
 <template>
-  <div>首页</div>
+  <div class="container">
+    <!-- 背景轮播图 -->
+      <el-carousel :interval="3000" arrow="always">
+         <el-carousel-item v-for="(item,index) in banners" :key="index">
+           <div class="banner-image"
+                :style="`background:url(${$axios.defaults.baseURL}${item.url}) center center no-repeat;
+                background-size:contain contain;`">
+           </div>
+         </el-carousel-item>
+      </el-carousel>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      banners:[]
+    }
+  },
+  methods:{
 
+  },
+  mounted(){
+    this.$axios({
+      url:'/scenics/banners'
+    }).then(res=>{
+      console.log(res);
+      // 解构赋值
+       const {data}=res.data;
+       this.banners=data;
+    })
+  }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
 
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
+}
 </style>
